95906517fd7d8bc7632f11f0f3838f750d656e6c
Ruan Gomes <143746245+Ruannes@users.noreply.github.com>
2024-11-30 16:20:23 -0300

    Update Venda.java

--- src/model/Venda.java
+++ src/model/Venda.java
@@ -36,3 +36,50 @@ public class Venda {
     }
 
 }
+
+ public Produto LerProduto() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Informe o código do produto: ");
+        int codigo = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Informe a descrição do produto: ");
+        String descricao = scanner.nextLine();
+
+        System.out.print("Informe o preço do produto: ");
+        double preco = scanner.nextDouble();
+        scanner.nextLine();
+
+        System.out.println("Categoria (1-ALIMENTICIO, 2-HIGIENE, 3-LIMPEZA, 4-HORTIFRUTI, 5-PADARIA): ");
+        Categoria categoria = Categoria.values()[scanner.nextInt() - 1];
+
+        return new Produto(codigo, descricao, preco, categoria);
+    }
+
+    public void AdicionarProduto(Produto produto, double quantidade) {
+        Item item = new Item(quantidadeItens + 1, produto, quantidade);
+        adicionarItem(item);
+    }
+
+    public void AdicionarProdutos() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Quantos produtos você deseja adicionar? ");
+        int numProdutos = scanner.nextInt();
+        scanner.nextLine();
+
+        for (int i = 0; i < numProdutos; i++) {
+            System.out.println("\nAdicionando produto " + (i + 1) + ":");
+            Produto produto = lerProduto();
+
+            System.out.print("Informe a quantidade para este produto: ");
+            double quantidade = scanner.nextDouble();
+            scanner.nextLine();
+
+            adicionarProduto(produto, quantidade);
+        }
+
+        System.out.println("\nProdutos adicionados com sucesso!");
+    }
+}
